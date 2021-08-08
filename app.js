452a70365c09cdc969cedb78d429fee30d6f3fa4
unknown <amazingsdk@gmail.com>
2021-08-08 19:04:15 -0400

    worked on creating a fun algorithm for upgrades.

--- app.js
+++ app.js
@@ -11,62 +11,58 @@ const shopListings = document.querySelectorAll('.shop-listing');
 const upgradeCost = document.querySelectorAll('.value');
 const shopContainer = document.querySelector('.shop-container');
 
-let score = 0;
+let score = 1000;
 let incrementAmt = 1;
 let autoIncrementAmt = 0;
-let luckUpgrade = false;
+let luckUpgradePercent = 0;
+let autoIncrementSpeed = 1000;
 
 scoreDisplay.textContent = `Score: ${score}`
 
 updateShopListings()
 
 // Update score
-let scoreUpdater = setInterval(incrementScore, 1000);
+let scoreUpdater = setInterval(incrementScore, autoIncrementSpeed);
 
 
 function applyUpgrades(upgradeNum) {
     switch (upgradeNum) {
+
+        // Add more points per click
         case 0:
-            console.log('Plus two each click!');
-            incrementAmt = 2;
-            scoreUpdater = setInterval(incrementScore, 500);
+            // Increase increment amount exponentially
+            incrementAmt *= 2;
+            // Update amount per click
+            shopListings[0].childNodes[1].textContent = `+${incrementAmt * 2} every click!`;
+            // scoreUpdater = setInterval(incrementScore, 500);
             break;
+
+        // Add more auto poppers
         case 1:
             console.log('Auto increment by 1 each second!');
-            autoIncrementAmt = 1;
-            scoreUpdater = setInterval(incrementScore, 250);
+            autoIncrementAmt += 1;
+            // scoreUpdater = setInterval(incrementScore, 250);
             break;
+            
+        // 10% chance of double points on click
         case 2:
             console.log('Plus three each click!');
-            incrementAmt = 3;
+            if (luckUpgradePercent < 0.96) {
+                if (luckUpgradePercent === 0) {
+                    luckUpgradePercent = 0.10;
+                } else {
+                    luckUpgradePercent += 0.05;
+                }
+                shopListings[2].childNodes[1].textContent = `${Math.round((luckUpgradePercent + 0.05)*100)}% chance of double points when popping!`;
+            }
             break;
+        
+        // Cut interval rate in half
         case 3:
-            console.log('Auto increment by 2 each second!');
-            autoIncrementAmt = 2;
-            scoreUpdater = setInterval(incrementScore, 125);
-            break;
-        case 4:
-            console.log('10% chance to double your click score!')
-            luckUpgrade = true;
-        case 5:
-            console.log('Plus five each click!');
-            incrementAmt = 5;
-            break;
-        case 6:
-            console.log('Auto increment by 3 each second!');
-            autoIncrementAmt = 3;
-            break;
-        case 7:
-            console.log('Plus eight each click!');
-            incrementAmt = 8;
-            break;
-        case 8:
-            console.log('Auto increment by five each second!');
-            autoIncrementAmt = 5;
-            break;
-        case 9:
-            console.log('Plus thirteen each click!');
-            incrementAmt = 13;
+            // console.log('Auto increment by 2 each second!');
+            // autoIncrementAmt = 2;
+            autoIncrementSpeed /= 2
+            scoreUpdater = setInterval(incrementScore, autoIncrementSpeed);
             break;
     }
 }
@@ -91,24 +87,43 @@ function updateShopListings() {
             elem.classList.remove('hide');
         }
 
+        if (score < shopItemPrice && !elem.classList.contains('hide')) {
+            elem.classList.add('line-through');
+        } else {
+            elem.classList.remove('line-through');
+        }
+
     });
 }
 
 shopListings.forEach( (elem, i) => {
 
-    // Upgrade cost
-    const elemCost = elem.childNodes[3].childNodes[1].textContent;
-
     // Upgrade listing
     // const elemListing = elem.parentNode;
 
+    const elemCostOriginal = elem.childNodes[3].childNodes[1].textContent;
+
     elem.addEventListener('click', () => {
+
+        // Upgrade cost
+        const elemCost = elem.childNodes[3].childNodes[1].textContent;
+        const elemAmt = elem.childNodes[5].childNodes[1].textContent;
+
         console.log('Hello World!', i);
+        console.log(elemCost)
         if (score >= Number(elemCost)) {
             score -= Number(elemCost);
+
+            // Calculate new upgrade price
+            elem.childNodes[3].childNodes[1].innerHTML = `${Math.ceil(elemCostOriginal * (1.1) ** (elemAmt+1)) + ((elemAmt+5)*10)}`;
+
+            // Update number of upgrades purchased
+            elem.childNodes[5].childNodes[1].innerHTML = `${Number(elemAmt) + 1}`
+
+            // console.log(elemCost)
             scoreDisplay.textContent = `Score: ${score}`
             elem.classList.add('line-through');
-            elem.childNodes[3].classList.add('hide');
+            // elem.childNodes[3].classList.add('hide');
             // elem.remove();
 
             applyUpgrades(i);
@@ -164,7 +179,8 @@ balloon.addEventListener('click', () => {
 
     const randInt = Math.random();
     console.log(randInt);
-    if (luckUpgrade && randInt < 0.10) {
+    if (randInt < luckUpgradePercent) {
+        console.log('DOUBLE SCORE!!!')
         html.classList.add('double-score');
         const doubleInc = incrementAmt * 2
         scoreDisplay.textContent = `Score: ${score += doubleInc}`;
